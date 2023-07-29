bcc20cbdea01f338a17f018f0a0b73b8ff6d9f2a
kimseungwoo12 <seungwookim1234@gmail.com>
2023-07-29 11:33:47 +0900

    논리연산 학습

--- operator.py
+++ operator.py
@@ -1,3 +1,5 @@
+# 기본 사칙 연산
+
 plus = 5 + 4
 minus = 10 - 7
 multiply = 20 * 3
@@ -9,7 +11,33 @@ print(multiply)
 print(division)
 
 r = 13 % 4
-n = 13//4
+n = 13 // 4
 
 print(r)
 print(n)
+
+#논리 연산
+numA = 20
+numB = 4
+
+isEqual = numA == numB
+isNotEqual = 5 != 4
+
+print(isEqual)
+print(isNotEqual)
+
+isEqual = numA == 20
+
+print(isEqual)
+
+isBiggerThan = numA > 20
+isBiggerOrEqualThan = numA >= 20
+
+print(isBiggerThan)
+print(isBiggerOrEqualThan)
+
+isSmallerThan = 20
+isSmallerOrEqualThan = 20
+
+print(isSmallerThan)
+print(isSmallerOrEqualThan)
