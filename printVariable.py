adefc373040afb9448b9356e4e7d4922452a25b6
kimseungwoo12 <seungwookim1234@gmail.com>
2023-07-29 10:56:42 +0900

    print와 변수 학습

---
+++ printVariable.py
@@ -0,0 +1,14 @@
+# 변수
+
+first_variable = 1
+second_variable = "안녕하세요"
+
+third_variable = first_variable + 2
+
+# 출력구문
+
+print(first_variable)
+print(second_variable)
+print("변수를 넣지 않고 출력을 할거예요.")
+print(1)
+
