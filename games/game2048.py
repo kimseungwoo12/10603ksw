12ba11ce7de252668469798ea9866be31aee0d28
kimseungwoo12 <seungwookim1234@gmail.com>
2023-11-18 11:55:57 +0900

    배경 그리기

--- games/game2048.py
+++ games/game2048.py
@@ -1,92 +1,72 @@
+import pygame
+
+colors = {
+    'white': (255, 255, 255),
+    'black': (0, 0, 0),
+    '-1': (200, 200, 200),
+    '2': (130, 177, 255),
+    '4': (68, 138, 255),
+    '8': (41, 121, 255),
+    '16': (41, 98, 255),
+    '32': (167, 255, 235),
+    '64': (100, 255, 218),
+    '128': (29, 233, 182),
+    '256': (0, 191, 165),
+    '512': (132, 255, 255),
+    '1024': (24, 255, 255),
+    '2048': (0, 229, 255),
+}
+
+board= [[-1, -1, -1, -1],
+         [-1, -1, -1, -1],
+         [-1, -1, -1, -1],
+         [-1, -1, -1, -1]]
+
+size = (500, 500)
+screen = pygame.display.set_mode(size)
+
+def initScreen():
+    screen.fill(colors['white'])
+    pygame.display.update()
+
+isGameRunning = True
+
+def setEventListener():
+    global isGameRunning
+    for event in pygame.event.get():
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_q:
+                isGameRunning = False
+            else:
+                print("키보드 키 입력 이벤트가 감지됨")
+
+def drawDisplay():
+    global screen
+
+    baseX = 35
+    baseY = 35
+    blockHeight = 100
+    blockWidth = 100
+    margin = 10
+
+    for i in range(4):
+        for j in range(4):
+            x = (blockWidth + margin) * j + baseX
+            y = (blockWidth + margin) * i + baseY
+            pygame.draw.rect(screen, colors['-1'], [x, y, blockHeight, blockWidth], 2)
+
+        pygame.display.flip()
+
+
 def run2048():
+    pygame.init()
+    initScreen()
     print("2048 게임 시작")
 
-    #### #### #### ####
-    #### #### #### ####
-    #### #### #### ####
-    #### #### #### ####
+    while isGameRunning:
+        setEventListener()
+        drawDisplay()
 
-    000
-    8
-    0016  #### ####
-    000
-    8
-    0004
-    0004
-    000
-    8
-    0512
-    012
-    8
-    0064
-    000
-    8
-    2048
-    1024
-    0512
-    012
-    8
+    pygame.quit()
 
-    1.
-    화면출력
-    모두  ####이고, 2와 4가 랜덤 위치에 뜨게
-    #### #### 0004 ####
-    #### 0002 #### ####
-    #### #### #### ####
-    #### #### #### ####
-
-    2.
-    사용자의
-    움직임이
-    있으면
-    그
-    방향으로
-    숫자를
-    밀고
-    빈칸
-    중
-    하나(랜덤위치)
-    에
-    2, 4
-    중에서
-    하나가
-    뜨게
-    3.
-    블럭이
-    한방향으로
-    모일때
-    해당
-    방향에
-    같은
-    숫자가
-    있으면
-    블럭
-    합치기
-
-    (아랫방향 입력시)
-    #### #### #### ####
-    #### #### 0002 ####
-    #### #### #### ####
-    #### 0002 0004 ####
-
-    (왼쪽방향 입력시)
-    #### #### #### ####
-    0002  #### 0002 ####
-    #### #### #### ####
-    0002
-    0004  #### ####
-
-    (아랫방향 입력시)
-    #### #### #### ####
-    0002  #### #### ####
-    #### #### #### ####
-    0004
-    0004  #### ####
-
-    4.
-    최종적으로
-    2048
-    이
-    만들어졌으면
-    WIN.
-    '''
+run2048()
